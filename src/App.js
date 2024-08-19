6e45e39e7f8859c7760c36c5ee28463b2f9dea78
trannghia204 <tranhuunghia204@gmail.com>
2024-08-19 18:59:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          web bán quần áo
         </a>
       </header>
     </div>
